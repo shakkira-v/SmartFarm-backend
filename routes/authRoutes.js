f7b8ac702157291d5ab2669708d629272fe59fbe
shakkira-v <shakkira111@gmail.com>
2026-04-26 05:16:01 +0530

    second2 commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-import { registerUser, loginUser, verifyEmail, forgotPassword, resetPassword } from "../controllers/AuthController.js";
+import { registerUser, loginUser, verifyEmail, forgotPassword, resetPassword } from "../controllers/authController.js";
 
 const router = express.Router();
 
